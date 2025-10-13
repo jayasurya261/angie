82f512d6c9846c87293f1c71321808585d4807bd
jayasurya <suryasunrise261@gmail.com>
2025-10-13 17:57:25 +0530

    patch work

--- src/sample.ts
+++ src/sample.ts
@@ -1,17 +1,15 @@
 const author = {
-    name: `Angela`,
-    nickname: `angie`,
-    tiktok: `https://www.tiktok.com/@codetonero`,
-    ig: `https://www.instagram.com/codetonero/`,
-    x: `https://x.com/codetonero`,
-    email: 'mailto:anthonylannn@gmail.com'
+    name: `Jayasurya`,
+    nickname: `surya`,
+    LinkedIn: `https://www.linkedin.com/in/jayasurya-r-b37997279`,
+    email: 'suryasunrise261@gmail.com'
 
 }
 
 
 const buy = {
     title: `See on Github`,
-    link: `https://github.com/anthonylan/angie`
+    link: `https://github.com/jayasurya261`
 }
 
 
